3711fb19fcccb8c0705df2c49cadac3821c1c112
ponomarev-vitaly <vp211082@gmail.com>
2024-12-04 14:48:12 -0500

    updated MainAutomationTests.java

--- src/test/java/MainAutomationTests.java
+++ src/test/java/MainAutomationTests.java
@@ -1,2 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MainAutomationTests {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+    }
+
 }
