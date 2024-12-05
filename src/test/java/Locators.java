5fa293f379342f8b92e62602d0de2f713b94d64b
ponomarev-vitaly <vp211082@gmail.com>
2024-12-04 23:53:56 -0500

    updated Locators.java

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -2,18 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Locators {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("hello123");
         driver.findElement(By.className("signInBtn")).click();
-
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-
-
-
 
     }
 }
