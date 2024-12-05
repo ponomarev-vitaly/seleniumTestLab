8d9d67326b9ac7d656bfafe6b86f65e6c5fa28cb
ponomarev-vitaly <vp211082@gmail.com>
2024-12-05 00:10:35 -0500

    updated Locators.java

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -14,6 +14,7 @@ public class Locators {
         driver.findElement(By.className("signInBtn")).click();
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
         driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Johnny");
 
     }
 }
