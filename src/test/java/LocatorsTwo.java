9cac08dd79c3d11863bf125dc265f85fd4d0f363
ponomarev-vitaly <vp211082@gmail.com>
2024-12-06 00:17:26 -0500

    updated LocatorsTwo.java

--- src/test/java/LocatorsTwo.java
+++ src/test/java/LocatorsTwo.java
@@ -6,13 +6,14 @@ import org.testng.Assert;
 import java.time.Duration;
 
 public class LocatorsTwo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
         driver.findElement(By.className("signInBtn")).click();
+        Thread.sleep(2000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 
