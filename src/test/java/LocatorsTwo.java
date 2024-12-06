a6dcae7bcd0d2abcc056fdd138364275efc465b1
ponomarev-vitaly <vp211082@gmail.com>
2024-12-06 00:08:40 -0500

    created LocatorsTwo.java, updated LocatorsTwo.java

---
+++ src/test/java/LocatorsTwo.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LocatorsTwo {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("signInBtn")).click();
+    }
+}
