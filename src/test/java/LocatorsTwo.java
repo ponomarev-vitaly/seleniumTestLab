3d34dd600d64a16cd334f25bd1c105f8f44b6358
ponomarev-vitaly <vp211082@gmail.com>
2024-12-06 00:28:31 -0500

    updated LocatorsTwo.java

--- src/test/java/LocatorsTwo.java
+++ src/test/java/LocatorsTwo.java
@@ -7,15 +7,16 @@ import java.time.Duration;
 
 public class LocatorsTwo {
     public static void main(String[] args) throws InterruptedException {
+        String name = "rahul";
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
         driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(2000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
-
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+name+",");
     }
 }
