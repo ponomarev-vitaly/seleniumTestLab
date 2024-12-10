956fb4f5efc9545efe4612076202a26aac06171a
ponomarev-vitaly <vp211082@gmail.com>
2024-12-10 04:41:07 -0500

    updated BrowserActivities.java

--- src/test/java/BrowserActivities.java
+++ src/test/java/BrowserActivities.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserActivities {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://google.com"); // Wait for all components are loaded
+        driver.navigate().to("https://rahulshettyacademy.com");
 
     }
 }
