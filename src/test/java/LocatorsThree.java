151aaaf6ba61ce7fbff8710d120612c887cb1217
ponomarev-vitaly <vp211082@gmail.com>
2024-12-10 04:28:58 -0500

    updated LocatorsThree.java

--- src/test/java/LocatorsThree.java
+++ src/test/java/LocatorsThree.java
@@ -1,2 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LocatorsThree {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        // Sibling -> Child, Child -> Sibling
+        // //header/div/button[1]/following-sibling::button[1]
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+    }
 }
