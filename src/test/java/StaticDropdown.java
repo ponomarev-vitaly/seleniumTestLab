31c4e09b1506440f0f87e3d49ee91afbc4749401
ponomarev-vitaly <vp211082@gmail.com>
2024-12-10 21:06:04 -0500

    updated StaticDropdown.java

--- src/test/java/StaticDropdown.java
+++ src/test/java/StaticDropdown.java
@@ -1,2 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class StaticDropdown {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+    }
 }
