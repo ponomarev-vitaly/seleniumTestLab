5ee1b77096fdd0f28089daef156f2bfcf7b0f3dc
ponomarev-vitaly <vp211082@gmail.com>
2024-12-10 21:12:32 -0500

    updated StaticDropdown.java

--- src/test/java/StaticDropdown.java
+++ src/test/java/StaticDropdown.java
@@ -1,10 +1,21 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class StaticDropdown {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        // Dropdown select with select tag
+        WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+
+        Select dropdown = new Select(staticDropdown);
+        dropdown.selectByIndex(3);
+
+
+
 
     }
 }
