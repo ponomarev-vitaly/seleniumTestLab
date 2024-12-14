6d77c181a125c4850812880fec841f7d991a34c9
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 04:27:29 -0500

    created new Java class Dropdown.java

--- src/test/java/Dropdown.java
+++ src/test/java/Dropdown.java
@@ -1,2 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Dropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("a[value='BLR']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+    }
 }
