34191d39f9c37f18d1b6de44a0d685baeecdc0a4
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 03:24:57 -0500

    updated UpdatedDropdown.java

--- src/test/java/UpdatedDropdown.java
+++ src/test/java/UpdatedDropdown.java
@@ -1,6 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class UpdatedDropdown {
 
     public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.spicejet.com/");
+        driver.findElement(By.id("divpaxinfo")).click();
+
 
     }
 }
