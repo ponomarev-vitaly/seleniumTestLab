2c88d20ae4cd94a0449f69cf0d1e76ce38070534
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 03:53:47 -0500

    updated UpdatedDropdown.java

--- src/test/java/UpdatedDropdown.java
+++ src/test/java/UpdatedDropdown.java
@@ -11,6 +11,7 @@ public class UpdatedDropdown {
         // driver.get("https://www.spicejet.com/");
         driver.findElement(By.id("divpaxinfo")).click();
         Thread.sleep(2000L);
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
         int i = 1;
         while(i<5){
             driver.findElement(By.id("hrefIncAdt")).click();
@@ -22,6 +23,8 @@ public class UpdatedDropdown {
 //        }
 
         driver.findElement(By.id("btnclosepaxoption")).click();
+
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 
     }
 }
