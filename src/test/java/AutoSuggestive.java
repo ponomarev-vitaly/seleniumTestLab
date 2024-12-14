270d9916753f26e5f2e191797c0ec0062954ee1d
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 05:03:51 -0500

    updated AutoSuggestive.java

--- src/test/java/AutoSuggestive.java
+++ src/test/java/AutoSuggestive.java
@@ -1,6 +1,14 @@
-public class AutoSuggestive {
-    public static void main(String[] args) {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class AutoSuggestive {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(3000);
 
     }
 }
