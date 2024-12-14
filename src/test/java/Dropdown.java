ee7f2d39779079f5b62fb43726ab17f1d331fc39
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 04:43:05 -0500

    updated Dropdown.java

--- src/test/java/Dropdown.java
+++ src/test/java/Dropdown.java
@@ -10,8 +10,10 @@ public class Dropdown {
 
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("a[value='BLR']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+//        driver.findElement(By.cssSelector("a[value='BLR']")).click();
+//        Thread.sleep(2000);
+//        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+
     }
 }
