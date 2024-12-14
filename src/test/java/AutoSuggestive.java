bfd930dce764266130e528409acd0fa357458933
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 05:09:52 -0500

    updated AutoSuggestive.java

--- src/test/java/AutoSuggestive.java
+++ src/test/java/AutoSuggestive.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class AutoSuggestive {
     public static void main(String[] args) throws InterruptedException {
@@ -9,6 +12,8 @@ public class AutoSuggestive {
 
         driver.findElement(By.id("autosuggest")).sendKeys("ind");
         Thread.sleep(3000);
+        List<WebElement> options = (List<WebElement>) driver.findElement(By.cssSelector("li[class='ui-menu-item'] a"));
+
 
     }
 }
