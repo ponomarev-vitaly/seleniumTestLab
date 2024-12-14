c4d1ec3df61606b5972f7e422fcc4a1182bb956c
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 03:35:11 -0500

    updated UpdatedDropdown.java

--- src/test/java/UpdatedDropdown.java
+++ src/test/java/UpdatedDropdown.java
@@ -4,15 +4,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UpdatedDropdown {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.spicejet.com/");
         driver.findElement(By.id("divpaxinfo")).click();
-        driver.findElement(By.id("hrefIncAdt")).click();
+        Thread.sleep(2000L);
+        int i = 1;
+        while(i<5){
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }
+
         driver.findElement(By.id("btnclosepaxoption")).click();
-
-
 
     }
 }
