b41c968cd050bd22ee897f9eb6aacb99f1755050
ponomarev-vitaly <vp211082@gmail.com>
2024-12-14 04:52:38 -0500

    updated Dropdown.java

--- src/test/java/Dropdown.java
+++ src/test/java/Dropdown.java
@@ -14,6 +14,8 @@ public class Dropdown {
 //        Thread.sleep(2000);
 //        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 
-
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[text()=' Bengaluru (BLR)']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[text()=' Chennai (MAA)']")).click();
     }
 }
