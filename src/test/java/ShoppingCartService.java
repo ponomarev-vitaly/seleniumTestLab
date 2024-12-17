08d8638bfbed2874bc2440a56e1a6cd45a1878c2
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 02:14:02 -0500

    updated ShoppingCartService.java

--- src/test/java/ShoppingCartService.java
+++ src/test/java/ShoppingCartService.java
@@ -1,2 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ShoppingCartService {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+
+    }
 }
