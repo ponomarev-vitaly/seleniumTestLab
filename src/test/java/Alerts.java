6d513821c9c75efe57f5fd5e0f0e096530ba10fe
ponomarev-vitaly <vp211082@gmail.com>
2024-12-16 23:47:31 -0500

    updated Alerts.java

--- src/test/java/Alerts.java
+++ src/test/java/Alerts.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Allerts {
+public class Alerts {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
