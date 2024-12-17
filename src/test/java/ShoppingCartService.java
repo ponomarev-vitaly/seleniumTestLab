2987269dbe6904e9d3d85e2ff0c91315faf3c7e1
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 06:39:05 -0500

    updated ShoppingCartService.java

--- src/test/java/ShoppingCartService.java
+++ src/test/java/ShoppingCartService.java
@@ -2,25 +2,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class ShoppingCartService {
     public static void main(String[] args) throws InterruptedException {
         String[] names = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         // Thread.sleep(3000L);
         addItems(driver, names);
         driver.findElement(By.cssSelector("img[alt='Cart']")).click();
         driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+
         driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
         driver.findElement(By.cssSelector("button.promoBtn")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
         System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
     }
 
