691f9024f2d8e183ff89f3876cdc9d5c6d4a819b
ponomarev-vitaly <vp211082@gmail.com>
2024-12-16 23:17:03 -0500

    updated UIE2ETestFramework.java

--- src/test/java/UIE2ETestFramework.java
+++ src/test/java/UIE2ETestFramework.java
@@ -1,2 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
 public class UIE2ETestFramework {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[text()=' Delhi (DEL)']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[text()=' Chennai (MAA)']")).click();
+
+        driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
+        if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1")){
+            System.out.println("The return date calendar is enabled...");
+            Assert.assertTrue(true);
+        }else {
+            Assert.assertTrue(false);
+        }
+
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+
+        int i = 1;
+        while(i<5){
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }
+
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+        WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+        Select dropdown = new Select(staticDropdown);
+        dropdown.selectByIndex(3);
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByVisibleText("USD");
+
+        //driver.findElement(By.cssSelector("#ctl00_mainContent_btn_FindFlights")).click();
+        driver.findElement(By.cssSelector("input[value='Search']")).click();
+
+    }
 }
