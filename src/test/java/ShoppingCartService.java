78b89c3dad3ab1c144703ef814a81fab43e97429
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 02:27:38 -0500

    updated ShoppingCartService.java

--- src/test/java/ShoppingCartService.java
+++ src/test/java/ShoppingCartService.java
@@ -1,11 +1,25 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ShoppingCartService {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
+        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+        for(int i =0; i<products.size(); i++){
+            String nameOfTheProduct = products.get(i).getText();
+
+            if(nameOfTheProduct.contains("Cucumber")){
+                driver.findElement(By.cssSelector("//button[text()='ADD TO CART']")).click();
+                break;
+            }
+        }
 
     }
 }
