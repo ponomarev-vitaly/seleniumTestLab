7350cf7aad78c47d3ff520015a749d68cc6d06e2
ponomarev-vitaly <vp211082@gmail.com>
2024-12-16 23:55:43 -0500

    updated Alerts.java

--- src/test/java/Alerts.java
+++ src/test/java/Alerts.java
@@ -3,10 +3,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Alerts {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String text = "Vitaliy";
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-        driver.findElement(By.id("name"));
-
+        driver.findElement(By.id("name")).sendKeys(text);
+        driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+        Thread.sleep(2000L);
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
     }
 }
