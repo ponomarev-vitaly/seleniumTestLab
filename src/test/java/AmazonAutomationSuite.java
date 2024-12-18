d84a774c5cc0af4ce45f59fa7cef61e22a583c1d
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 00:53:26 -0500

    updated AmazonAutomationSuite.java

--- src/test/java/AmazonAutomationSuite.java
+++ src/test/java/AmazonAutomationSuite.java
@@ -8,32 +8,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class AmazonAutomationSuite {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("(//span[@class='a-button-inner'])[1]")));
-
-        //driver.findElement(By.xpath("(//span[@class='a-button-inner'])[1]")).click();
 
         Actions a = new Actions(driver);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='nav-link-accountList-nav-line-1']")));
         WebElement moveToElementHello = driver.findElement(By.xpath("//span[@id='nav-link-accountList-nav-line-1']"));
         a.moveToElement(moveToElementHello).build().perform();
-
-        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
-
-
-
-
-
 
     }
 }
