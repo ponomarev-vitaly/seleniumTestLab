10dd643e39555efa3b3a8d4ec6814310e6d133e1
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 23:40:49 -0500

    updated AmazonAutomationSuite.java

--- src/test/java/AmazonAutomationSuite.java
+++ src/test/java/AmazonAutomationSuite.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class Actions {
+public class AmazonAutomationSuite {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        Actions a = new Actions(driver);
+
 
     }
 }
