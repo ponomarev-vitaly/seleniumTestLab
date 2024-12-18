3f4543443088190220846ad6e4432c9bbbc90a9f
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 20:26:09 -0500

    updated FluentWaitTest.java

--- src/test/java/FluentWaitTest.java
+++ src/test/java/FluentWaitTest.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
+import java.util.function.Function;
 
 public class FluentWaitTest {
     public static void main(String[] args) {
@@ -16,6 +18,14 @@ public class FluentWaitTest {
         Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(3))
                 .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector("[id='finish'] h4"));
+            }
+        });
+
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
 
 
     }
