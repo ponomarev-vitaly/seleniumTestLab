9beba0fdeda67d30e3262cb7a3f99c57afd28c60
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 20:11:03 -0500

    updated FluentWaitTest.java

--- src/test/java/FluentWaitTest.java
+++ src/test/java/FluentWaitTest.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FluentWaitTest {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
 
 
     }
