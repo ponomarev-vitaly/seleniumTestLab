07286fffc5f675544896f02483493a2f85315997
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 03:44:53 -0500

    updated FramesTest.java

--- src/test/java/FramesTest.java
+++ src/test/java/FramesTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FramesTest {
     public static void main(String[] args) {
@@ -9,6 +11,10 @@ public class FramesTest {
         driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
         // driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
         driver.findElement(By.id("draggable")).click();
+        Actions a = new Actions(driver);
+        WebElement sourceElement = driver.findElement(By.id("draggable"));
+        WebElement targetElement = driver.findElement(By.id("droppable"));
+        a.dragAndDrop(sourceElement, targetElement);
 
 
     }
