24ed8ecfc6c44559433756996486754d9a025211
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 03:59:00 -0500

    updated Assignment5.java

--- src/test/java/Assignment5.java
+++ src/test/java/Assignment5.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment5 {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.cssSelector("a[href='/nested_frames']")).click();
 
     }
 }
