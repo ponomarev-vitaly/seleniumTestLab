93fb34f06d274320409d09e6f47e113ac2c4571e
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 03:26:06 -0500

    updated FramesTest.java

--- src/test/java/FramesTest.java
+++ src/test/java/FramesTest.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FramesTest {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/droppable/");
+        driver.findElement(By.id("draggabl")).click();
 
     }
 }
