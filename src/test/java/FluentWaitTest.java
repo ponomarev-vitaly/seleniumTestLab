63b57b651d84b92866ea1fdbcbad850113e7dccf
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 20:34:12 -0500

    updated FluentWaitTest.java

--- src/test/java/FluentWaitTest.java
+++ src/test/java/FluentWaitTest.java
@@ -21,12 +21,13 @@ public class FluentWaitTest {
 
         WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
             public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.cssSelector("[id='finish'] h4"));
+                if (driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()){
+                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
+                } else
+                    return null;
             }
         });
 
-        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
-
-
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
     }
 }
