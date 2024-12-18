49adb3da96f0fd947e7e4e9fef6791bf96e669d1
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 02:30:43 -0500

    updated Assignment4.java

--- src/test/java/Assignment4.java
+++ src/test/java/Assignment4.java
@@ -1,11 +1,35 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Assignment4 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/windows");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+        driver.findElement(By.cssSelector("a[href='/windows/new']")).click();
+
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> stringIterator = windows.iterator();
+        String parentId = stringIterator.next();
+        String childId = stringIterator.next();
+
+        driver.switchTo().window(childId);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='example'] h3")));
+        String textInChildWindow = driver.findElement(By.cssSelector("div[class='example'] h3")).getText();
+        System.out.println("The text from the child window: " + textInChildWindow);
+
+        driver.switchTo().window(parentId);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='example'] h3")));
+        String textInParentWindow = driver.findElement(By.cssSelector("div[class='example'] h3")).getText();
+        System.out.println("The text from the parent window: " + textInParentWindow);
 
 
     }
