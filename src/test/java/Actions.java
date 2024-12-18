e5ee6b5d0a993c3dc8c59916813fba3634d31680
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 23:35:50 -0500

    updated Actions.java

--- src/test/java/Actions.java
+++ src/test/java/Actions.java
@@ -1,2 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Actions {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+    }
 }
