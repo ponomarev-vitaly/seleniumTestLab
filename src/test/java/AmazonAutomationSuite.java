d603ff25564b40ca2607833c5d892f3c015b5ae4
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 00:46:47 -0500

    updated AmazonAutomationSuite.java

--- src/test/java/AmazonAutomationSuite.java
+++ src/test/java/AmazonAutomationSuite.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class AmazonAutomationSuite {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
@@ -21,10 +23,13 @@ public class AmazonAutomationSuite {
         //driver.findElement(By.xpath("(//span[@class='a-button-inner'])[1]")).click();
 
         Actions a = new Actions(driver);
-        WebElement moveToElementHello = driver.findElement(By.xpath("//span[@id='nav-link-accountList-nav-line-1']"));
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='nav-link-accountList-nav-line-1']")));
+        WebElement moveToElementHello = driver.findElement(By.xpath("//span[@id='nav-link-accountList-nav-line-1']"));
         a.moveToElement(moveToElementHello).build().perform();
+
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+
 
 
 
