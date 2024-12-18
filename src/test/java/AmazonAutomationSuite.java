752938eb0c20355a432d61ebc03e3fdf79cba165
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 00:24:41 -0500

    updated AmazonAutomationSuite.java

--- src/test/java/AmazonAutomationSuite.java
+++ src/test/java/AmazonAutomationSuite.java
@@ -2,13 +2,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class AmazonAutomationSuite {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("(//span[@class='a-button-inner'])[1]")));
+
+        //driver.findElement(By.xpath("(//span[@class='a-button-inner'])[1]")).click();
+
         Actions a = new Actions(driver);
-        a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='nav-link-accountList-nav-line-1']")));
+        a.moveToElement(driver.findElement(By.xpath("//span[@id='nav-link-accountList-nav-line-1']"))).build().perform();
 
 
 
