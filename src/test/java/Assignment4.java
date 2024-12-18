d8d5e5dfeef3816710c5931f8338615ff633ceb7
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 02:08:46 -0500

    updated Assignment4.java

--- src/test/java/Assignment4.java
+++ src/test/java/Assignment4.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment4 {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+
 
     }
 }
