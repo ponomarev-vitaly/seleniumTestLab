ba9a9acc28f6b0af19e994d00a071df073af6efe
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 01:05:41 -0500

    updated WindowHandles.java

--- src/test/java/WindowHandles.java
+++ src/test/java/WindowHandles.java
@@ -2,11 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class WindowHandles {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/loginpagePractise/");
         driver.findElement(By.cssSelector(".blinkingText")).click();
+        Set<String> windows = driver.getWindowHandles();
 
 
 
