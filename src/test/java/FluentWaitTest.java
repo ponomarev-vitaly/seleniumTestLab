957069f426fe37dcda61b215896b57a43dade538
ponomarev-vitaly <vp211082@gmail.com>
2024-12-17 20:13:50 -0500

    updated FluentWaitTest.java

--- src/test/java/FluentWaitTest.java
+++ src/test/java/FluentWaitTest.java
@@ -1,12 +1,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 public class FluentWaitTest {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
         driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3));
 
 
     }
