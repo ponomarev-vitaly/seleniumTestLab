4f71670a388152a6a3d4a10f8371944cdb7d5840
ponomarev-vitaly <vp211082@gmail.com>
2024-12-18 22:35:25 -0500

    updated Scope.java

--- src/test/java/Scope.java
+++ src/test/java/Scope.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scope {
@@ -9,7 +10,13 @@ public class Scope {
 
         driver.get("https://qaclickacademy.com/practice.php");
         System.out.println(driver.findElements(By.tagName("a")).size());
+
+        // Count links only in the footer
+
         // driver.findElements(By.tagName("a")).size();
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        System.out.println(footerDriver.findElements(By.tagName("a")).size());
+        // footerDriver.findElements(By.tagName("a")).size();
 
     }
 }
