99f404f499b44f8504b36a1155614632dd3a7f2d
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 06:24:27 -0500

    updated JavaScriptExecutor.java

--- src/test/java/JavaScriptExecutor.java
+++
@@ -1,5 +0,0 @@
-public class JavaScriptExecutor {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/JavaScriptExecutorTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutorTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+
+
+    }
+}
