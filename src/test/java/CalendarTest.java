97865df5cc0b651ab4f8d8519b6d4f44f1a6caa6
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 04:27:49 -0500

    updated CalendarTest.java

--- src/test/java/CalendarTest.java
+++ src/test/java/CalendarTest.java
@@ -1,5 +1,12 @@
 public class CalendarTest {
     public static void main(String[] args) {
+        String monthNumber = "6";
+        String date = "15";
+        String year = "2027";
+
+
+
+
 
     }
 }
