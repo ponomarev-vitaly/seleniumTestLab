c04ed8826c93385d6035b3d8f0efea38ff3b3827
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 06:36:01 -0500

    updated JavaScriptExecutor.java

--- src/test/java/JavaScriptExecutorTest.java
+++ src/test/java/JavaScriptExecutorTest.java
@@ -3,10 +3,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollBy(0,500)");
+        Thread.sleep(3000L);
+        js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 
 
 
