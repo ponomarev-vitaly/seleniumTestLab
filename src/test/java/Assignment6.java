0e967ed172a8812bd000d8e5fbe7498b039e4e7b
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 02:25:18 -0500

    updated Assignment6.java

--- src/test/java/Assignment6.java
+++ src/test/java/Assignment6.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Assignment6 {
     public static void main(String[] args) {
@@ -9,9 +11,20 @@ public class Assignment6 {
         driver.get("https://qaclickacademy.com/practice.php");
 
         String verificationText = driver.findElement(By.cssSelector("label[for='honda']")).getText();
-        System.out.println(verificationText);
+        // System.out.println(verificationText);
 
         driver.findElement(By.xpath("(//input[@id='checkBoxOption3'])[1]")).click();
+
+        WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+        Select abc = new Select(dropdown);
+        abc.selectByVisibleText(verificationText);
+
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys(verificationText);
+        driver.findElement(By.xpath("//input[@id='alertbtn']")).click();
+
+
+
+
 
 
 
