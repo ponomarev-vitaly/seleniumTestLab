189e067c996d9d7ef8c99e18eec3dd3e37072218
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 02:18:13 -0500

    updated Assignment6.java

--- src/test/java/Assignment6.java
+++ src/test/java/Assignment6.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,6 +7,13 @@ public class Assignment6 {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://qaclickacademy.com/practice.php");
+
+        String verificationText = driver.findElement(By.cssSelector("label[for='honda']")).getText();
+        System.out.println(verificationText);
+
+        driver.findElement(By.xpath("(//input[@id='checkBoxOption3'])[1]")).click();
+
+
 
     }
 }
