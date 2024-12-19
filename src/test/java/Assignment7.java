61bc4ba2da05d973b0f7bc870c0cfc8d0ac483d3
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 07:32:33 -0500

    updated Assignment7.java

--- src/test/java/Assignment7.java
+++ src/test/java/Assignment7.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment7 {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+
 
     }
 }
