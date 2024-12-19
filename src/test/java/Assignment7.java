5e3dda1bc72d2d7f62b9e712473ddcb7183d2928
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 07:34:18 -0500

    updated Assignment7.java

--- src/test/java/Assignment7.java
+++ src/test/java/Assignment7.java
@@ -1,10 +1,18 @@
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assignment7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollBy(0,750)");
+        Thread.sleep(3000L);
+
+
 
 
 
