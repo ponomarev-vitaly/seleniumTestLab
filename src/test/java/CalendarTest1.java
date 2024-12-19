be5def2e8c6917057334ed9ee629965a64fcb99a
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 05:16:24 -0500

    updated CalendarTest1.java

--- src/test/java/CalendarTest1.java
+++ src/test/java/CalendarTest1.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,13 @@ public class CalendarTest1 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
+        driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).click();
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
+
+        driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
+        driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber) - 1).click();
+        driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
 
     }
 }
