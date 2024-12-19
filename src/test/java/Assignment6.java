06cf0dfdd59ba1c7627b6dff244d234eefd552e3
ponomarev-vitaly <vp211082@gmail.com>
2024-12-19 01:57:15 -0500

    updated Assignment6.java

--- src/test/java/Assignment6.java
+++ src/test/java/Assignment6.java
@@ -1,2 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment6 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://qaclickacademy.com/practice.php");
+
+    }
 }
