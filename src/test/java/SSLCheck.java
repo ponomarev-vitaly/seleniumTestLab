890ee6cb203bef5bc31d98b8d8e7c10d6b0095aa
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 04:34:54 -0500

    updated SSLCheck.java

--- src/test/java/SSLCheck.java
+++ src/test/java/SSLCheck.java
@@ -7,16 +7,19 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class SSLCheck {
     public static void main(String[] args) {
         ChromeOptions options = new ChromeOptions();
+
         options.setAcceptInsecureCerts(true);
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://expired.badssl.com/");
         System.out.println(driver.getTitle());
 
-        FirefoxOptions options1 = new FirefoxOptions();
-        options1.setAcceptInsecureCerts(true);
-        WebDriver driver1 = new FirefoxDriver(options1);
-        driver1.get("https://expired.badssl.com/");
-        System.out.println(driver1.getTitle());
+//        FirefoxOptions options1 = new FirefoxOptions();
+//        options1.setAcceptInsecureCerts(true);
+//        WebDriver driver1 = new FirefoxDriver(options1);
+//        driver1.get("https://expired.badssl.com/");
+//        System.out.println(driver1.getTitle());
+
+
 
 
     }
