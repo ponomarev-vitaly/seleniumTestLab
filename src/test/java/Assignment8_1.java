5f8780876f7fc847cb2a33e27e5a512da5d87b73
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 03:15:00 -0500

    created Assignment8_1.java

--- src/test/java/Assignment8_1.java
+++ src/test/java/Assignment8_1.java
@@ -1,5 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assignment8_1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://qaclickacademy.com/practice.php");
+        driver.findElement(By.id("autocomplete")).sendKeys("ind");
+        Thread.sleep(2000);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
 
     }
 }
