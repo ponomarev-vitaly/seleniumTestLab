ecd58912ddf19e719756d26169834da11f0b3d85
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 03:08:04 -0500

    updated Assignment8.java

--- src/test/java/Assignment8.java
+++ src/test/java/Assignment8.java
@@ -3,8 +3,11 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 
 public class Assignment8 {
@@ -15,16 +18,16 @@ public class Assignment8 {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
         WebElement selectTheCountry = driver.findElement(By.xpath("//input[@id='autocomplete']"));
 
         js.executeScript("window.scrollBy(0,50)");
-        Thread.sleep(3000L);
 
         selectTheCountry.sendKeys("uni");
-        Thread.sleep(3000L);
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ui-menu-item")));
         js.executeScript("document.querySelector('.ui-menu-item').scrollBy(0,50)");
         js.pin("document.querySelector('.ui-menu-item-wrapper').scrollBy(0,50)");
 
@@ -45,13 +48,6 @@ public class Assignment8 {
         System.out.println("THe country found after parsing the input form: " + selectTheCountry.getAttribute("value"));
 
         Assert.assertEquals(selectTheCountry.getAttribute("value"), countryFound);
-
-
-//        js.executeScript("document.querySelector('.ui-menu-item').scrollBy(0,50)");
-//        js.pin("document.querySelector('.ui-menu-item-wrapper').scrollBy(0,50)");
-//        String firstEelement = (String) js.executeScript("document.querySelector('.ui-menu-item-wrapper').textContent");
-//        System.out.println(firstEelement);
-
 
     }
 }
