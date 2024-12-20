7e5818ed3009950429df09f0252d92b85a32e02e
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 06:04:26 -0500

    updated BrokenLinks.java

--- src/test/java/BrokenLinks.java
+++ src/test/java/BrokenLinks.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrokenLinks {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("");
+        String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+
 
     }
 }
