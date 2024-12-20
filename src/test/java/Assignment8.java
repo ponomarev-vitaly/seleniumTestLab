a2258898d0d432a023b3a6f2af10f7850ba48205
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 03:01:56 -0500

    updated Assignment8.java

--- src/test/java/Assignment8.java
+++ src/test/java/Assignment8.java
@@ -3,13 +3,16 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.List;
 
 public class Assignment8 {
     public static void main(String[] args) throws InterruptedException {
-        String countryToSearchFor = "";
+        String countryToSearchFor = "United Kingdom (UK)";
+        String countryToSearchFor1;
         String countryFound = "";
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -21,17 +24,27 @@ public class Assignment8 {
 
         selectTheCountry.sendKeys("uni");
         Thread.sleep(3000L);
+
+        js.executeScript("document.querySelector('.ui-menu-item').scrollBy(0,50)");
+        js.pin("document.querySelector('.ui-menu-item-wrapper').scrollBy(0,50)");
+
         List<WebElement> listOfCountries = driver.findElements(By.cssSelector(".ui-menu-item .ui-menu-item-wrapper"));
         for (int i = 0; i < listOfCountries.size(); i++) {
-            countryToSearchFor = listOfCountries.get(i).getText();
-            if (countryToSearchFor.equals("United Kingdom (UK)")) {
-                countryFound = countryToSearchFor;
-                System.out.println("There is a match: " + countryToSearchFor);
+
+            countryToSearchFor1 = listOfCountries.get(i).getText();
+            if (countryToSearchFor1.equals(countryToSearchFor)) {
+                listOfCountries.get(i).click();
+                System.out.println("There is a match: " + countryToSearchFor1);
+                countryFound = countryToSearchFor1;
+
             }
 
         }
 
-        System.out.println(countryFound);
+        System.out.println("The country found after looping the list: " + countryFound);
+        System.out.println("THe country found after parsing the input form: " + selectTheCountry.getAttribute("value"));
+
+        Assert.assertEquals(selectTheCountry.getAttribute("value"), countryFound);
 
 
 //        js.executeScript("document.querySelector('.ui-menu-item').scrollBy(0,50)");
