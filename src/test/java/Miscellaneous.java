621575df31a08a609b0e1e5f8f00ffa6b803c5b3
ponomarev-vitaly <vp211082@gmail.com>
2024-12-20 04:46:46 -0500

    updated Miscellaneous.java

--- src/test/java/Miscellaneous.java
+++ src/test/java/Miscellaneous.java
@@ -1,2 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Miscellaneous {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+    }
 }
