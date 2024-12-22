1ca0cc97e88c724f371baf656b31280aba7e4db2
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 02:16:53 -0500

    updated StreamProcessingExamples.java

--- src/test/java/StreamProcessingExamples.java
+++ src/test/java/StreamProcessingExamples.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class StreamProcessingExamples {
     public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("James");
+        names.add("Raymond");
+        names.add("Joshua");
+        names.add("Alexander");
+        names.add("Alice");
+        names.add("Anna");
+        names.add("Andrea");
+        names.add("Aurora");
+        names.add("Abigail");
 
 
     }
