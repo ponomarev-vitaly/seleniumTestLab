2e703b2ce98701b098c554d54e225b66dbacc3d2
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 03:02:43 -0500

    updated AdvancedMapOperations.java

--- src/test/java/AdvancedMapOperations.java
+++ src/test/java/AdvancedMapOperations.java
@@ -9,9 +9,9 @@ public class AdvancedMapOperations {
         // Choose&print names which have last letter "a" with Uppercase
 
         Stream.of("James", "Raymond", "Joshua", "Alexander", "Alice", "Anna", "Andrea", "Aurora", "Abigail")
-                .filter(s->s.endsWith("a"))
-                .map(s->s.toUpperCase())
-                .forEach(s-> System.out.println(s));
+                .filter(s -> s.endsWith("a"))
+                .map(s -> s.toUpperCase())
+                .forEach(s -> System.out.println(s));
 
 
     }
