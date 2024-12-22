365eef61aee466ea297ce635bc6f5e1d0f8602ca
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 01:24:02 -0500

    updated BrokenLinks.java

--- src/test/java/BrokenLinks.java
+++ src/test/java/BrokenLinks.java
@@ -1,19 +1,30 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class BrokenLinks {
     public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        List<WebElement> links = driver.findElements(By.cssSelector("li[class=\"gf-li\"] a"));
+        for (WebElement link : links) {
+            String url = link.getAttribute("href");
+
+            HttpURLConnection connectToWeb = (HttpURLConnection) new URL(url).openConnection();
+            connectToWeb.setRequestMethod("HEAD");
+            connectToWeb.connect();
+            int responseCode = connectToWeb.getResponseCode();
+            System.out.println(responseCode);
+        }
+
         String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
-
         HttpURLConnection connectToWeb = (HttpURLConnection) new URL(url).openConnection();
-
         connectToWeb.setRequestMethod("HEAD");
         connectToWeb.connect();
         int responseCode = connectToWeb.getResponseCode();
