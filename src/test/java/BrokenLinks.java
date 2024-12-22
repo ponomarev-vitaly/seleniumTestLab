26d9f648eccf34259662b745c08b80dd696d043f
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 01:09:20 -0500

    updated BrokenLinks.java

--- src/test/java/BrokenLinks.java
+++ src/test/java/BrokenLinks.java
@@ -2,11 +2,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class BrokenLinks {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.get("");
         String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+
+        HttpURLConnection connectToWeb = (HttpURLConnection) new URL(url).openConnection();
+
+        connectToWeb.setRequestMethod("HEAD");
+        connectToWeb.connect();
 
 
     }
