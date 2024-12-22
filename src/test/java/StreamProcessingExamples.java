dcd17a511e67da81cadc04f93f029debfad41ec9
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 02:27:06 -0500

    updated StreamProcessingExamples.java

--- src/test/java/StreamProcessingExamples.java
+++ src/test/java/StreamProcessingExamples.java
@@ -1,7 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 
 public class StreamProcessingExamples {
-    public static void main(String[] args) {
+    @Test
+    public void regular() {
         ArrayList<String> names = new ArrayList<String>();
         names.add("James");
         names.add("Raymond");
@@ -22,7 +25,21 @@ public class StreamProcessingExamples {
         }
 
         System.out.println(count);
+    }
 
+    @Test
+    public void streamFilter(){
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("James");
+        names.add("Raymond");
+        names.add("Joshua");
+        names.add("Alexander");
+        names.add("Alice");
+        names.add("Anna");
+        names.add("Andrea");
+        names.add("Aurora");
+        names.add("Abigail");
 
+        names.stream().filter(s->s.startsWith("A"));
     }
 }
