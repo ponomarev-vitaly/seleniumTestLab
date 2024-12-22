00b3d3554a843d321a73cca404e195b1aee4dcd6
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 02:56:05 -0500

    updated AdvancedMapOperations.java

--- src/test/java/AdvancedMapOperations.java
+++ src/test/java/AdvancedMapOperations.java
@@ -6,6 +6,7 @@ public class AdvancedMapOperations {
 
     @Test
     public void streamMap() {
+        // Choose&print names of length > 4 with Uppercase
         Stream.of("James", "Raymond", "Joshua", "Alexander", "Alice", "Anna", "Andrea", "Aurora", "Abigail");
 
 
