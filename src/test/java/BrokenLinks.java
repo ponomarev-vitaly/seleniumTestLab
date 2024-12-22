7c15baf44a8af21d3d1fa18994c7f79c439b85e9
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 01:37:59 -0500

    updated BrokenLinks.java

--- src/test/java/BrokenLinks.java
+++ src/test/java/BrokenLinks.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
