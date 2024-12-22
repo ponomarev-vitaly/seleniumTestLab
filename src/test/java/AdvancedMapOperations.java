e87f9748533a65fd1ba8b071e0dbcf98c821a2a4
ponomarev-vitaly <vp211082@gmail.com>
2024-12-22 03:10:07 -0500

    updated AdvancedMapOperations.java

--- src/test/java/AdvancedMapOperations.java
+++ src/test/java/AdvancedMapOperations.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class AdvancedMapOperations {
@@ -8,10 +10,11 @@ public class AdvancedMapOperations {
     public void streamMap() {
         // Choose&print names which have last letter "a" with Uppercase
 
-        Stream.of("James", "Raymond", "Joshua", "Alexander", "Alice", "Anna", "Andrea", "Aurora", "Abigail")
-                .filter(s -> s.endsWith("a"))
-                .map(s -> s.toUpperCase())
-                .forEach(s -> System.out.println(s));
+        Stream.of("James", "Raymond", "Joshua", "Alexander", "Alice", "Anna", "Andrea", "Aurora", "Abigail").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
+
+        // Choose&print names which have first letter "a" with Uppercase and sort it
+        List<String> names = Arrays.asList("James", "Raymond", "Joshua", "Alexander", "Alice", "Anna", "Andrea", "Aurora", "Abigail");
+        names.stream().filter(s -> s.startsWith("A")).sorted().map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
 
 
     }
