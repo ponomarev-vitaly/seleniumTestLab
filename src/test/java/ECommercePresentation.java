c614b8aadc4a6e69f8cbf7aa0e6126969ed6ee31
ponomarev-vitaly <vp211082@gmail.com>
2024-12-23 08:17:31 -0500

    updated ECommercePresentation.java

--- src/test/java/ECommercePresentation.java
+++ src/test/java/ECommercePresentation.java
@@ -1,5 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ECommercePresentation {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
 
     }
 }
