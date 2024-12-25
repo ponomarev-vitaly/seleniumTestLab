b704579cbca4eb634593a6d4fa649e2dd3341620
ponomarev-vitaly <vp211082@gmail.com>
2024-12-24 22:45:53 -0500

    updated ECommerceFilter.java

--- src/test/java/ECommerceFilter.java
+++ src/test/java/ECommerceFilter.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ECommerceFilter {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.findElement(By.id("search-field"));
+
+
 
     }
 }
