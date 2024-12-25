e19c8059ddce4c24961b43d0c23aaccd2fefe83f
ponomarev-vitaly <vp211082@gmail.com>
2024-12-25 11:30:09 -0500

    updated MultipleWindows.java

--- src/test/java/MultipleWindows.java
+++ src/test/java/MultipleWindows.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,6 +20,8 @@ public class MultipleWindows {
 
         driver.switchTo().window(childWindow);
         driver.get("https://rahulshettyacademy.com/");
+        String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+        // System.out.println(courseName);
 
 
     }
