ba6f7c189569f3e0181d8e2d314c7cee45fb51a7
ponomarev-vitaly <vp211082@gmail.com>
2024-12-24 23:00:51 -0500

    updated ECommerceFilter.java

--- src/test/java/ECommerceFilter.java
+++ src/test/java/ECommerceFilter.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +15,7 @@ public class ECommerceFilter {
         List<WebElement> veggies = driver.findElements(By.xpath("'//tr/td[1]"));
         // 5 results
         List<WebElement> filteredList = veggies.stream().filter(veggie->veggie.getText().contains("Rice")).collect(Collectors.toList());
-
+        Assert.assertEquals(veggies.size(), filteredList.size());
 
     }
 }
