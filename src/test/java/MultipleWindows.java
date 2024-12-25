3a0da67b1a189fb6d62d8dde5be0e733b655e0ca
ponomarev-vitaly <vp211082@gmail.com>
2024-12-25 11:37:00 -0500

    updated MultipleWindows.java

--- src/test/java/MultipleWindows.java
+++ src/test/java/MultipleWindows.java
@@ -11,7 +11,8 @@ public class MultipleWindows {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
 
-        driver.switchTo().newWindow(WindowType.TAB);
+        // driver.switchTo().newWindow(WindowType.TAB);
+        driver.switchTo().newWindow(WindowType.WINDOW);
 
         Set<String> windowHandles = driver.getWindowHandles();
         Iterator<String> it = windowHandles.iterator();
@@ -23,8 +24,8 @@ public class MultipleWindows {
         String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
         // System.out.println(courseName);
         driver.switchTo().window(parentWindow);
-
-
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+        // driver.quit();
 
 
     }
