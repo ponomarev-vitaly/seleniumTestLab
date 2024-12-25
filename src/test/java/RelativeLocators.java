720a8b0e02b14c754ca423fb9364154852dbc7d1
ponomarev-vitaly <vp211082@gmail.com>
2024-12-24 23:18:49 -0500

    updated RelativeLocators.java

--- src/test/java/RelativeLocators.java
+++ src/test/java/RelativeLocators.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RelativeLocators {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+
 
     }
 }
