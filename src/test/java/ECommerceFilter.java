cc7a4fd400b95ee7d058249c079b9608e0c705b3
ponomarev-vitaly <vp211082@gmail.com>
2024-12-24 22:55:10 -0500

    updated ECommerceFilter.java

--- src/test/java/ECommerceFilter.java
+++ src/test/java/ECommerceFilter.java
@@ -1,13 +1,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ECommerceFilter {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
         driver.findElement(By.id("search-field")).sendKeys("Rice");
-
+        List<WebElement> veggies = driver.findElements(By.xpath("'//tr/td[1]"));
+        // 5 results
+        veggies.stream().filter(veggie->veggie.getText().contains("Rice")).collect(Collectors.toList());
 
 
     }
