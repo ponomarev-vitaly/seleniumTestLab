1170158d6e678dbc6099b9c7bb5b63ad34efe0ca
ponomarev-vitaly <vp211082@gmail.com>
2024-12-25 11:19:28 -0500

    updated MultipleWindows.java

--- src/test/java/MultipleWindows.java
+++ src/test/java/MultipleWindows.java
@@ -2,12 +2,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class MultipleWindows {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
 
         driver.switchTo().newWindow(WindowType.TAB);
+
+        Set<String> windowHandles = driver.getWindowHandles();
+        // driver.switchTo().window(nameOrHandle);
+
 
     }
 }
